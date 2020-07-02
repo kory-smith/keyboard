8d2fe11f955f2aac9b8f09b6a0f744061a6a1497
Kory Smith <kor54e@gmail.com>
2020-07-02 18:15:54 -0400

    Remove shift from hotkey for use in "modes" bindings

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -7,7 +7,7 @@ end
 for i, mapping in ipairs(hyperModeAppMappings) do
   local key = mapping[1]
   local app = mapping[2]
-  hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, key, function()
+  hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, key, function()
     if (type(app) == 'string') then
       hs.application.open(app)
     elseif (type(app) == 'function') then
