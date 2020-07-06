0f6e732464bad90e5114b2d771e6224fb000fcbe
Kory Smith <kor54e@gmail.com>
2020-07-06 13:47:15 -0400

    Add notification dismissal utility binding

--- hammerspoon/utils.lua
+++ hammerspoon/utils.lua
@@ -12,4 +12,23 @@ hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "o", function() hs.osascript.app
 		display notification "URI " & uri & " copied to clipboard"
 	end if
 end tell
-]])end)
+]])
+end)
+
+hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "d", function() hs.osascript.applescript([[
+	my closeNotifications()
+on closeNotifications()
+	tell application "System Events" to tell process "Notification Center"
+		set theWindows to every window
+		repeat with i from 1 to number of items in theWindows
+			set this_item to item i of theWindows
+			try
+				click button 1 of this_item
+			on error
+				my closeNotifications()
+			end try
+		end repeat
+	end tell
+end closeNotifications
+]])
+end)
