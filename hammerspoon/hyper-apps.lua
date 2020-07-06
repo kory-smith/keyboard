6e44c48082cf6ae90959f307aa9ec92a68b8d4d2
Kory Smith <kor54e@gmail.com>
2020-07-06 09:55:02 -0400

    Add Kory's apps to the hyper-apps file

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,19 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+
+
+return {
+  -- { 'a', 'Music' },             -- "A" for "Apple Music"
+  { 'b', 'Brave Browser' },             -- "B" for "Browser"
+  { 'c', 'Visual Studio Code' },             -- "C for "Code"
+  {'t', 'Todoist'},
+  -- { 'd', (function() print(type("hey"))}, -- "D" for "Do!" ... or "Done!"
+  -- { 'e', 'Atom' },              -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'm', 'Microsoft Outlook' },       -- "M" for "eMail"
+  { 'n', 'Microsoft Teams' },             -- "S" for "Slack"
+  -- { 't', 'todoist' },             -- "T" for "Terminal"
+}
