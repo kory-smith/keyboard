dfd4de194f415611bfc56973ab4439f46d6e7d07
Kory Smith <kor54e@gmail.com>
2020-07-07 20:38:54 -0400

    Remove ctrl + u to delete to beginning of line

--- hammerspoon/delete-words.lua
+++ hammerspoon/delete-words.lua
@@ -30,10 +30,10 @@ end)
 -- we don't need (or want) this hotkey in the terminal. If this hotkey was
 -- enabled in the terminal, it would break the standard control + u behavior.
 -- Therefore, we only enable this hotkey for non-terminal apps.
-local wf = hs.window.filter.new():setFilters({iTerm2 = false, Terminal = false})
-enableHotkeyForWindowsMatchingFilter(wf, hs.hotkey.new({'ctrl'}, 'u', function()
-  keyUpDown({'cmd'}, 'delete')
-end))
+-- local wf = hs.window.filter.new():setFilters({iTerm2 = false, Terminal = false})
+-- enableHotkeyForWindowsMatchingFilter(wf, hs.hotkey.new({'ctrl'}, 'u', function()
+--   keyUpDown({'cmd'}, 'delete')
+-- end))
 
 -- Use control + ; to delete to end of line
 --
