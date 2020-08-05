3192d8bef99ef5d1390c7044a4c496ada8a0803b
Kory Smith <kory@Ret.local>
2020-08-05 13:33:43 -0400

    Add VPN handling bindings.

--- hammerspoon/utils.lua
+++ hammerspoon/utils.lua
@@ -1,5 +1,6 @@
 
 
+-- Dismisses all notifications
 hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "d", function() hs.osascript.applescript([[
 	my closeNotifications()
 on closeNotifications()
@@ -18,4 +19,22 @@ end closeNotifications
 ]])
 end)
 
+-- Locks the screen
 hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "l", function() hs.caffeinate.lockScreen() end)
+
+-- Connects to VPN
+hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "v", function() hs.osascript.applescript([[
+	tell application "Tunnelblick"
+	connect "TorGuard.USA-CHICAGO"
+end tell
+]])
+end)
+
+-- Disconnects from VPN
+hs.hotkey.bind({'ctrl', 'shift', 'cmd', 'alt'}, "n", function() hs.osascript.applescript([[
+	tell application "Tunnelblick"
+	disconnect "TorGuard.USA-CHICAGO"
+end tell
+]])
+hs.notify.new({title="VPN Disconnected"}):send()
+end)
