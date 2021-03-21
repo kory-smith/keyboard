91ed8f60a390d9fcbde10af1faf2bf539621f9c6
Kory Smith <kory@Ret.lan>
2021-03-21 16:48:09 -0400

    Change hyper-apps.lua depending on computer host name

--- hammerspoon/hyper-apps.lua
+++ hammerspoon/hyper-apps.lua
@@ -4,21 +4,37 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 
-
-return {
-  -- { 'a', 'Music' },             -- "A" for "Apple Music"
-  { 'c', 'Microsoft Teams' },             -- "c" for "chat"
-  { 'b', 'Google Chrome' },             -- "B" for "Browser"
+local bindingsList = {
+  { 'b', 'Brave Browser' },             -- "B" for "Browser"
+  { 'c', 'Docker Desktop' },             -- "c" for "Containers"
+  { 'd', 'Dashlane' },             -- "d" for "Dashlane"
   { 'e', 'Visual Studio Code' },             -- "E" for "editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 'g', 'Google Chrome' },             -- "g" for "Google"
+  { 'n', 'Notion' },            -- "n" for "Notebook"
+  { 'p', 'Pushbullet' },             -- "p" for "Pushbullet"
   { 't', 'Todoist'},
   { 'y', 'YNAB' },            -- "Y" for "YNAB"
-  { 'n', 'Notion' },            -- "n" for "Notebook"
-  { 'f', 'Finder' },            -- "F" for "Finder"
-  { 's', 'Spotify' },            -- "S" for "Spotify"
-  { 'm', 'Microsoft Outlook' },       -- "M" for "eMail"
-  { 'o', 'Microsoft OneNote' },       -- "O" for "OneNote"
-  { 'p', 'Pushbullet' },             -- "g" for "Google"
-  { 'x', 'Microsoft Excel' },             -- "x" for "Excel"
-  { 'z', 'zoom.us' },             -- "z" for "Zoom"
   { 'return', 'iTerm' },
 }
+
+if hs.host.localizedName() == "OF014X1C3UJG5JI" then
+  bindingsList = {
+    { 'c', 'Microsoft Teams' },             -- "c" for "chat"
+    { 'b', 'Google Chrome' },             -- "B" for "Browser"
+    { 'e', 'Visual Studio Code' },             -- "E" for "editor"
+    { 't', 'Todoist'},
+    { 'y', 'YNAB' },            -- "Y" for "YNAB"
+    { 'n', 'Notion' },            -- "n" for "Notebook"
+    { 'f', 'Finder' },            -- "F" for "Finder"
+    { 's', 'Spotify' },            -- "S" for "Spotify"
+    { 'm', 'Microsoft Outlook' },       -- "M" for "eMail"
+    { 'o', 'Microsoft OneNote' },       -- "O" for "OneNote"
+    { 'p', 'Pushbullet' },             -- "g" for "Google"
+    { 'x', 'Microsoft Excel' },             -- "x" for "Excel"
+    { 'z', 'zoom.us' },             -- "z" for "Zoom"
+    { 'return', 'iTerm' },
+  }
+end
+
+return bindingsList
