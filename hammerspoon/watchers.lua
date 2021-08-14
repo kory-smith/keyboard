3e4c2e273ba6d45aee85ec8b252227f59c258264
Kory Smith <kor54e@gmail.com>
2021-08-14 17:56:31 -0400

    Add auto VPN connection if not on home network

--- hammerspoon/watchers.lua
+++ hammerspoon/watchers.lua
@@ -1,3 +1,31 @@
+local engageWithTunnel = function(connectOrDisconnect)
+    local _, status = hs.osascript.applescript([[
+        tell application "Tunnelblick"
+        get state of first configuration
+        end tell
+        ]])
+    if (connectOrDisconnect == "connect" and status == "EXITING") then
+        hs.osascript.applescript([[
+            tell application "Tunnelblick"
+            get name of first configuration
+            connect result
+            repeat until result = "CONNECTED"
+              delay 1
+              get state of first configuration
+            end repeat
+          end tell
+        ]])
+    end
+    if (connectOrDisconnect == "disconnect" and status == "CONNECTED") then
+        hs.osascript.applescript([[
+            tell application "Tunnelblick"
+            get name of first configuration
+            disconnect result
+          end tell
+        ]])
+    end
+end
+
 return {
     mouseWatcher = hs.usb.watcher.new(function(deviceTable)
         if (deviceTable.eventType == "added" and deviceTable.productName == "Microsoft Pro Intellimouse") then
@@ -5,6 +33,16 @@ return {
             if deviceTable.eventType == "added" and deviceTable.productName == "Logitech::USB Receiver" then
                 hs.mouse.trackingSpeed(0.6875)
             end
+        end
+    end),
+
+    wifiWatcher = hs.wifi.watcher.new(function(watcher, a, b)
+        if (hs.wifi.currentNetwork() ~= "Pie-fi-5G" and hs.network.interfaceDetails() ~= "nil")
+        or (hs.wifi.currentNetwork() ~= "Platypus Cave 5G" and hs.network.interfaceDetails() ~= "nil") then
+            engageWithTunnel("connect")
+        end
+        if (hs.wifi.currentNetwork() == "Pie-fi-5G") or (hs.wifi.currentNetwork() == "Platypus Cave 5G") then
+            engageWithTunnel("disconnect")
         end
     end)
 }
