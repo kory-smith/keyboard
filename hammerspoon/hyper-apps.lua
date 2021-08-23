a77b6656418d9e8600e66dae2c5008893cdf657a
Kory Smith <kor54e@gmail.com>
2021-08-23 12:03:39 -0400

    Make hyper app bindings DRY-er

--- hammerspoon/hyper-apps.lua
+++ hammerspoon/hyper-apps.lua
@@ -5,13 +5,19 @@
 -- your preferred shortcuts.
 
 local bindingsList = {
-  { 'b', 'Brave Browser' },             -- "B" for "Browser"
-  { 'c', 'Google Chrome' },             -- "c" for "Containers"
-  { 'd', 'Dashlane' },             -- "d" for "Dashlane"
-  { 'e', 'Visual Studio Code' },             -- "E" for "editor"
-  { 'f', 'Finder' },            -- "F" for "Finder"
-  { 'n', 'Notion' },            -- "n" for "Notebook"
-  { 't', 'Todoist'},
+  { 'e', 'Visual Studio Code' },      -- "E" for "editor"
+  { 'f', 'Finder' },                  -- "F" for "Finder"
+  { 'g', 'Fork' },                    -- "G" for "Git GUI"
+  { 'n', 'Notion' },                  -- "N" for "Notion"
+  { 't', 'Todoist'},                  -- "T" for "Todoist"
+  { 'y', 'YNAB' },                    -- "Y" for "YNAB"
+  { 'return', 'iTerm' },
+}
+
+local personalBindings = {
+  { 'b', 'Brave Browser' },           -- "B" for "Browser"
+  { 'c', 'Google Chrome' },           -- "C" for "Chrome"
+  { 'd', 'Dashlane' },                -- "D" for "Dashlane"
   {'x', function()
     if hs.application.get("OpenOffice") then
         hs.application.launchOrFocus("OpenOffice")
@@ -21,28 +27,24 @@ local bindingsList = {
             hs.eventtap.keyStroke(null, "s")
         end)
     end
-end}, -- "Y" for "YNAB"
-  { 'y', 'YNAB' },            -- "Y" for "YNAB"
-  { 'return', 'iTerm' },
+end},
 }
 
--- Work computer bindings
+local workBindings = {
+  { 'b', 'Google Chrome' },             -- "B" for "Browser"
+  { 'c', 'Microsoft Teams' },           -- "C" for "Chat"
+  { 'm', 'Microsoft Outlook' },         -- "M" for "eMail"
+  { 'p', 'Join' },             
+  { 't', 'Todoist'},
+  { 'x', 'Microsoft Excel' },           -- "x" for "Excel"
+  { 'z', 'zoom.us' },                   -- "Z" for "Zoom"
+}
+
+-- Work computer name
 if hs.host.localizedName() == "OF014X1C3UJG5JI" then
-  bindingsList = {
-    { 'b', 'Google Chrome' },             -- "B" for "Browser"
-    { 'c', 'Microsoft Teams' },             -- "c" for "chat"
-    { 'e', 'Visual Studio Code' },             -- "E" for "editor"
-    { 'f', 'Finder' },            -- "F" for "Finder"
-    { 'g', 'Fork' },             -- "E" for "editor"
-    { 'm', 'Microsoft Outlook' },       -- "M" for "eMail"
-    { 'n', 'Notion' },            -- "n" for "Notebook"
-    { 'p', 'Join' },             -- "g" for "Google"
-    { 't', 'Todoist'},
-    { 'x', 'Microsoft Excel' },             -- "x" for "Excel"
-    { 'y', 'YNAB' },            -- "Y" for "YNAB"
-    { 'z', 'zoom.us' },             -- "z" for "Zoom"
-    { 'return', 'iTerm' },
-  }
+  bindingsList = hs.fnutils.concat(bindingsList, workBindings)  
+else
+  bindingsList = hs.fnutils.concat(bindingsList, personalBindings)  
 end
 
 return bindingsList
