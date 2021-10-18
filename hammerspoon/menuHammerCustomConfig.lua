b828a4bf0d03dc642b11315c30a84aae64d05b89
Kory Smith <kor54e@gmail.com>
2021-10-18 16:18:01 -0400

    Let clear notifications work on Speedy too

--- hammerspoon/menuHammerCustomConfig.lua
+++ hammerspoon/menuHammerCustomConfig.lua
@@ -809,7 +809,50 @@ menuHammerMenuList = {
                   end}
             }},
             {cons.cat.action, '', 'C', "Clear notifications", {
-                {cons.act.func, function() hs.osascript.applescript([[
+                {cons.act.func, function() 
+                    if hs.host.localizedName() == "Speedy" then
+                    hs.osascript.applescript([[
+                        activate application "NotificationCenter"
+                            tell application "System Events"
+                                tell process "Notification Center"
+                                repeat
+                                    try
+                                        set theWindow to group 1 of UI element 1 of scroll area 1 of window "Notification Center"
+                                    on error
+                                        exit repeat
+                                    end try
+                                    
+                                    try
+                                        set theActions to actions of theWindow
+                                        
+                                        # Try to close the whole group first. If that fails, close individual windows.
+                                        repeat with theAction in theActions
+                                            if description of theAction is "Clear All" then
+                                                set closed to true
+                                                tell theWindow
+                                                    perform theAction
+                                                end tell
+                                                exit repeat
+                                            end if
+                                        end repeat
+                                        
+                                        repeat with theAction in theActions
+                                            if description of theAction is "Close" then
+                                                set closed to true
+                                                tell theWindow
+                                                    perform theAction
+                                                end tell
+                                                exit repeat
+                                            end if
+                                        end repeat
+                                        
+                                    end try
+                                end repeat
+                            end tell
+                        end tell
+                    ]])
+else
+                    hs.osascript.applescript([[
                     my closeNotifications()
                 on closeNotifications()
                     tell application "System Events" to tell process "Notification Center"
@@ -825,6 +868,7 @@ menuHammerMenuList = {
                     end tell
                 end closeNotifications
                 ]])
+            end
                   end}
             }},
             {cons.cat.action, '', 'K', "Kill teams and outlook", {
